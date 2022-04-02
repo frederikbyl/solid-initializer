8ad4278d92e92fcce9ce63a1403d80abaf4166df
Bram Boeckx <bram.boeckx@student.uhasselt.be>
2022-04-02 15:54:58 +0200

    vcard implementatie

--- src/app/data/data.component.ts
+++ src/app/data/data.component.ts
@@ -12,19 +12,19 @@ import { v4 } from 'uuid';
 })
 export class DataComponent implements OnInit {
   session: any;
-  name='';
-  city='';
-  street='';
-  houseNumber='';
-  gsm='';
-  dataLocation='private/address/'
+  name = '';
+  city = '';
+  street = '';
+  houseNumber = '';
+  gsm = '';
+  dataLocation = 'private/address/'
 
   constructor(private router: Router) { }
 
   ngOnInit(): void {
     if (getDefaultSession().info.isLoggedIn) {
       this.session = getDefaultSession();
-   
+
     } else {
       this.router.navigate(['/login']);
     }
@@ -42,19 +42,17 @@ export class DataComponent implements OnInit {
 
     const addressThing = buildThing(createThing({ name: myuuid }))
       .addUrl(RDF.type, "http://vito.be/contactGegevens")
-      .addStringNoLocale("http://vito.be/naam",this.name)
-      .addStringNoLocale("http://vito.be/straat",this.street)
-      .addStringNoLocale("http://vito.be/huisNummer",this.houseNumber)
-      .addStringNoLocale("http://vito.be/gsm",this.gsm)
+      .addStringNoLocale("http://vito.be/naam", this.name)
+      .addStringNoLocale("http://vito.be/straat", this.street)
+      .addStringNoLocale("http://vito.be/huisNummer", this.houseNumber)
+      .addStringNoLocale("http://vito.be/gsm", this.gsm)
       .build();
-      addressDataset = setThing(addressDataset, addressThing);
-      
+    addressDataset = setThing(addressDataset, addressThing);
 
     saveSolidDatasetAt(this.getRootName() + this.dataLocation + myuuid, addressDataset, {
       fetch: this.session.fetch
     }).then((result) => {
       console.log("Data created");
-    
     });
 
 
@@ -62,17 +60,42 @@ export class DataComponent implements OnInit {
 
   saveAsVCARD() {
     //TODO: implement by Bram. I already imported the VCARD module from RDF lib.
-  }
+    console.log("Saving as vcard");
+    console.log(this.name);
+    console.log(this.city);
+    console.log(this.street);
+    console.log(this.houseNumber);
+    console.log(this.gsm);
+    console.log(this.dataLocation);
+    const uuid = v4();
+    let addressDataset = createSolidDataset();
 
-  saveAsSchemaOrg() {
-    //TODO: implement by Bram. I don't think there exists a module like vcard for this. You can write it as strings
-  }
+    const addressThing = buildThing(createThing({ name: uuid }))
+      .addUrl(RDF.type, VCARD.Individual)
+      .addStringNoLocale(VCARD.fn, this.name)
+      .addStringNoLocale(VCARD.adr, this.street + " " + this.houseNumber)
+      .addStringNoLocale(VCARD.locality, this.city)
+      .addStringNoLocale(VCARD.tel, this.gsm)
+      .build();
+    addressDataset = setThing(addressDataset, addressThing);
 
-  getRootName() {
-    if(getDefaultSession().info.isLoggedIn) {
-      return getDefaultSession().info.webId!.replace('profile/card#me','');
-    } else {
-      throw new Error("Not logged in");
-    }
+    saveSolidDatasetAt(this.getRootName() + this.dataLocation + uuid, addressDataset, {
+      fetch: this.session.fetch
+    }).then((result) => {
+      console.log("Data created");
+      console.log(uuid);
+    });
+}
+
+saveAsSchemaOrg() {
+  //TODO: implement by Bram. I don't think there exists a module like vcard for this. You can write it as strings
+}
+
+getRootName() {
+  if (getDefaultSession().info.isLoggedIn) {
+    return getDefaultSession().info.webId!.replace('profile/card#me', '');
+  } else {
+    throw new Error("Not logged in");
   }
+}
 }
